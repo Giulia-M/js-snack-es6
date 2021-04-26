a39cbf5e8ad0108cfc5f02203f31da8139a8ec35
Giulia-M <giuliamaggio16@gmail.com>
2021-04-26 17:51:52 +0200

    bozza

---
+++ es2/myScript.js
@@ -0,0 +1,52 @@
+/*
+Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0. Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti.
+Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+*/
+
+var squadre = [
+    {
+        nome: "squadra1",
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: "squadra2",
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: "squadra3",
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: "squadra4",
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+]
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+var specificheSquadra = [];
+
+for (let i = 0; i < squadre.length; i++) {
+    const squadraDaAnalizzare = squadre[i];
+    squadraDaAnalizzare.puntiFatti = getRandomInt(1, 50)
+    squadraDaAnalizzare.falliSubiti = getRandomInt(1, 50);
+
+    //destructuring di un object
+    var { nome, falliSubiti } = squadraDaAnalizzare
+    specificheSquadra.push({ nome, falliSubiti })
+    console.log(` ${nome}  ${falliSubiti} `);
+
+}
+console.log(squadre)
+console.log(specificheSquadra)
+
+
+
+
+
